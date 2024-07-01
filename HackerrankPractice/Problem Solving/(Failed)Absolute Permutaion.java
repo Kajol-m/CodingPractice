8304d566f8ff567bdd3a862b79122f9814cfdd70
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-07-01 23:55:02 +0530

    Create (Failed)Absolute Permutaion.java

---
+++ HackerrankPractice/Problem Solving/(Failed)Absolute Permutaion.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'absolutePermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     */
+
+    public static List<Integer> absolutePermutation(int n, int k) {
+    // Write your code here
+    List<Integer> perm=new ArrayList<Integer>();
+    for(int i=0;i<n;i++){
+        perm.add(i+1);
+    }
+    List<Integer> result=new ArrayList<Integer>();
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n;j++){
+            if(Math.abs(i-perm.get(j))==k){
+                result.add(perm.get(j));
+            }
+        }
+    }
+    
+    if(result.size()==n){
+        return result;
+    }
+    else{
+        result.removeAll(result);
+        result.add(-1);
+        return result;
+    }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> result = Result.absolutePermutation(n, k);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
