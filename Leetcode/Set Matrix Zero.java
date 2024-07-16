61fb9936c55691ca31f563a3859043b73c924acf
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-07-17 00:55:24 +0530

    Create Set Matrix Zero.java

---
+++ Leetcode/Set Matrix Zero.java
@@ -0,0 +1,22 @@
+    int m = matrix.length;
+    int n = matrix[0].length;
+    boolean[] rows = new boolean[m];
+    boolean[] columns = new boolean[n];
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == 0){
+                rows[i] = true;
+                columns[j] = true;
+            }
+        }
+    }
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(rows[i] || columns[j]){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
