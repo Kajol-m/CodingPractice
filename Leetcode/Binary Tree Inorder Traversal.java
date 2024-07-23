904d113b36b86c49071396884cd81d12e833cc06
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-07-23 13:34:41 +0530

    Update Binary Tree Inorder Traversal.java

--- Leetcode/Binary Tree Inorder Traversal.java
+++ Leetcode/Binary Tree Inorder Traversal.java
@@ -43,4 +43,16 @@ class Solution {
 }
 
 }
+
+class Solution {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root!= null) {
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+    }
+    return result;
+    }
+}
 */
