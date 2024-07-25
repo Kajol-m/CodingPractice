d92aaa13da78372027b883e2760aa60bc31d5e33
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-07-25 20:04:24 +0530

    Update Stack using Queue.java

--- Leetcode/Stack using Queue.java
+++ Leetcode/Stack using Queue.java
@@ -33,3 +33,32 @@ class MyStack {
         return q.isEmpty();
     }
 }
+/*
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q= new LinkedList<>();
+        
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for (int i = 1; i < q.size(); i++) q.add(q.poll());
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+*/
